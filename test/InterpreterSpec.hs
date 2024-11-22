2b1ab4535122b1ed9a83f445f001343929630d11
Andrés Alejandro Coronel Rodrigues <78624635+ACR1209@users.noreply.github.com>
2024-11-22 15:45:55 -0500

    <documentation> Refactor and reorder for readability (#6)
    * <documentation/refactor> reorder parser functions for better readability, documented parser and refactore to user parsec between function
    
    * <documentation> Enhance documentation for interpreter functions and types
    
    * <refactor> separate the spec of the parser and the interpreter
    
    * <refactor/test> Update valueToString to format lists with commas and enhance tests coverage
    
    * <fix> Update valueToString to format lists with a comma and space

--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -1,129 +1,13 @@
+module InterpreterSpec where
+
 import Test.Hspec
 import SimpleParser
 import Interpreter
 import Data.Either (isLeft)
 import qualified Data.Map as Map
 
-main :: IO ()
-main = hspec $ do
-  describe "SimpleParser" $ do
-    describe "Basic operations" $ do
-      it "should parse variable assigment" $ do
-        parseProgram "x = 42" `shouldBe` Right [Assign "x" (IntLit 42)]
-      it "should parse addition" $ do
-        parseProgram "x = 42 + 10" `shouldBe` Right [Assign "x" (Add (IntLit 42) (IntLit 10))]
-      it "should parse subtraction" $ do
-        parseProgram "x = 42 - 10" `shouldBe` Right [Assign "x" (Sub (IntLit 42) (IntLit 10))]
-      it "should parse multiplication" $ do
-        parseProgram "x = 42 * 10" `shouldBe` Right [Assign "x" (Mul (IntLit 42) (IntLit 10))]
-      it "should parse division" $ do
-        parseProgram "x = 42 / 10" `shouldBe` Right [Assign "x" (Div (IntLit 42) (IntLit 10))]
-      it "should parse string concatenation" $ do
-        parseProgram "x = \"hello\" + \"world\"" `shouldBe` Right [Assign "x" (Add (StrLit "hello") (StrLit "world"))]
-      it "should parse expression with parentheses" $ do
-        parseProgram "x = (42 + 10) * 2" `shouldBe` Right [Assign "x" (Mul (Add (IntLit 42) (IntLit 10)) (IntLit 2))]
-      it "should parse the true boolean" $ do
-        parseProgram "x = true" `shouldBe` Right [Assign "x" (BoolLit True)]
-      it "should parse the false boolean" $ do
-        parseProgram "x = false" `shouldBe` Right [Assign "x" (BoolLit False)]
-
-    describe "If statement" $ do
-      it "should parse if statement" $ do
-        parseProgram "if x { print x } else { print \"no x\" }" `shouldBe` Right [If (Var "x") [Print (Var "x")] [Print (StrLit "no x")]]
-      it "should parse if statement with multiple statements" $ do
-        parseProgram "if x { print x print x } else { print \"no x\" print \"no x\" }" `shouldBe` Right [If (Var "x") [Print (Var "x"), Print (Var "x")] [Print (StrLit "no x"), Print (StrLit "no x")]]
-      it "should parse if statement with nested if" $ do
-        parseProgram "if x { if y { print x } else { print y } } else { print \"no x\" }" `shouldBe` Right [If (Var "x") [If (Var "y") [Print (Var "x")] [Print (Var "y")]] [Print (StrLit "no x")]]
-      it "should parse if statement with no else" $ do
-        parseProgram "if x { print x }" `shouldBe` Right [If (Var "x") [Print (Var "x")] []]
-    
-    describe "Print statement" $ do
-      it "should parse print statement" $ do
-        parseProgram "print x" `shouldBe` Right [Print (Var "x")]
-      it "should give error when print statement used without parameter" $ do
-        parseProgram "print" `shouldSatisfy` isLeft 
-
-    describe "Comparison operators" $ do
-      it "should parse equality operator (==) for integers" $ do
-        parseProgram "x = 42 == 42" `shouldBe` Right [Assign "x" (Eq (IntLit 42) (IntLit 42))]
-      it "should parse equality operator (==) for strings" $ do
-        parseProgram "x = \"hello\" == \"hello\"" `shouldBe` Right [Assign "x" (Eq (StrLit "hello") (StrLit "hello"))]
-      it "should parse inequality operator (!=) for integers" $ do
-        parseProgram "x = 42 != 43" `shouldBe` Right [Assign "x" (Neq (IntLit 42) (IntLit 43))]
-      it "should parse inequality operator (!=) for strings" $ do
-        parseProgram "x = \"hello\" != \"world\"" `shouldBe` Right [Assign "x" (Neq (StrLit "hello") (StrLit "world"))]
-      it "should parse less than operator (<) for integers" $ do
-        parseProgram "x = 42 < 43" `shouldBe` Right [Assign "x" (Lt (IntLit 42) (IntLit 43))]
-      it "should parse greater than operator (>) for integers" $ do
-        parseProgram "x = 43 > 42" `shouldBe` Right [Assign "x" (Gt (IntLit 43) (IntLit 42))]
-      it "should parse less than or equal operator (<=) for integers" $ do
-        parseProgram "x = 42 <= 43" `shouldBe` Right [Assign "x" (Le (IntLit 42) (IntLit 43))]
-      it "should parse greater than or equal operator (>=) for integers" $ do
-        parseProgram "x = 43 >= 42" `shouldBe` Right [Assign "x" (Ge (IntLit 43) (IntLit 42))]
-
-    describe "List operations" $ do
-      it "should parse list literal" $ do
-        parseProgram "x = [1, 2, 3]" `shouldBe` Right [Assign "x" (ListLit [IntLit 1, IntLit 2, IntLit 3])]
-      it "should parse list access" $ do
-        parseProgram "x = [1, 2, 3][0]" `shouldBe` Right [Assign "x" (ListAccess (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 0))]
-      it "should parse list append" $ do
-        parseProgram "x = [1, 2, 3] << 4" `shouldBe` Right [Assign "x" (ListAppend (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 4))]
-      it "should parse list remove" $ do
-        parseProgram "x = [1, 2, 3] >> 2" `shouldBe` Right [Assign "x" (ListRemove (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 2))]
-      it "should parse list pop" $ do
-        parseProgram "x = [1, 2, 3] =>> 0" `shouldBe` Right [Assign "x" (ListPop (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 0))]
-      it "should parse list add" $ do
-        parseProgram "x = [1, 2, 3] << 0 <<= 4" `shouldBe` Right [Assign "x" (ListAdd (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 0) (IntLit 4))]
-
-    describe "Comments" $ do
-      it "should parse single line comment" $ do
-        parseProgram "# This is a comment" `shouldBe` Right [Comment "This is a comment"]
-      it "should parse multi line comment" $ do
-        parseProgram "/* This is a comment */" `shouldBe` Right [MultiLineComment "This is a comment"]
-      it "should parse multi line comment with new lines" $ do
-        parseProgram "/* This is a \n comment */" `shouldBe` Right [MultiLineComment "This is a \n comment"]
-      it "should parse a comment with a # in it" $ do
-        parseProgram "# This is a comment with a # in it" `shouldBe` Right [Comment "This is a comment with a # in it"]
-      it "should parse a comment with a /* in it" $ do
-        parseProgram "# This is a comment with a /* in it" `shouldBe` Right [Comment "This is a comment with a /* in it"]
-      it "should parse a comment with a */ in it" $ do
-        parseProgram "# This is a comment with a */ in it" `shouldBe` Right [Comment "This is a comment with a */ in it"]        
-      it "should correctly parse inline comments on the side of a statement" $ do
-        parseProgram "x = 42 # This is a comment" `shouldBe` Right [Assign "x" (IntLit 42), Comment "This is a comment"]
-
-    describe "Function definition" $ do
-      it "should parse a function definition" $ do
-        parseProgram "def add(x, y) { return x + y }" `shouldBe` Right [FuncDef "add" ["x", "y"] [Return (Add (Var "x") (Var "y"))]]
-
-      it "should fail if the function definition is missing the return keyword" $ do
-        parseProgram "def add(x, y) { x + y }" `shouldSatisfy` isLeft
-      
-      it "should fail if the return keyword is used outside a function" $ do
-        parseProgram "return 42" `shouldSatisfy` isLeft
-
-    describe "Function call" $ do
-      it "should parse a function call as a statement" $ do
-        parseProgram "add(1, 2)" `shouldBe` Right [FuncCall "add" [IntLit 1, IntLit 2]]
-      it "should parse a function call as an expression" $ do
-        parseProgram "x = add(1, 2)" `shouldBe` Right [Assign "x" (FuncCall "add" [IntLit 1, IntLit 2])]
-
-    describe "Logical operations" $ do
-      it "should be able to parse the logical and operator (and)" $ do
-        parseProgram "x = true and false" `shouldBe` Right [Assign "x" (LogicAnd (BoolLit True) (BoolLit False))]
-      it "should be able to parse the logical or operator (or)" $ do
-        parseProgram "x = true or false" `shouldBe` Right [Assign "x" (LogicOr (BoolLit True) (BoolLit False))]
-      it "should be able to parse the logical not operator (not)" $ do
-        parseProgram "x = not true" `shouldBe` Right [Assign "x" (LogicNot (BoolLit True))]
-
-    describe "For loop" $ do
-      it "should parse a for loop" $ do
-        parseProgram "for (x = 0; x < 2; x = x + 1) {}" `shouldBe` Right [ForLoop (Assign "x" (IntLit 0)) (Lt (Var "x") (IntLit 2)) (Assign "x" (Add (Var "x") (IntLit 1))) []]
-
-    describe "Import module" $ do
-      it "should parse an import module statement" $ do
-        parseProgram "import module" `shouldBe` Right [ImportModule "module"]
-
-  describe "Interpreter" $ do
+spec :: Spec
+spec = context "Interpreter" $ do
     describe "Basic operations" $ do
       it "should evaluate integer assignment" $ do
           let exprs = [Assign "x" (IntLit 42)]
@@ -201,30 +85,90 @@ main = hspec $ do
           result `shouldBe` Left "Type mismatch in division"
 
     describe "If statement" $ do
-      it "should evaluate if statement with true condition" $ do
-          let exprs = [Assign "x" (IntLit 1), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
-          result <- runInterpreter Map.empty exprs
-          result `shouldBe` Right (IntVal 0, Map.fromList [("x", IntVal 1), ("y", IntVal 42)])
+        it "should evaluate if statement with true condition" $ do
+            let exprs = [Assign "x" (IntLit 1), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
+            result <- runInterpreter Map.empty exprs
+            result `shouldBe` Right (IntVal 0, Map.fromList [("x", IntVal 1), ("y", IntVal 42)])
 
-      it "should evaluate with bools" $ do
-          let exprs = [Assign "x" (BoolLit True), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
-          result <- runInterpreter Map.empty exprs
-          result `shouldBe` Right (IntVal 0, Map.fromList [("x", BoolVal True), ("y", IntVal 42)])
+        it "should evaluate with bools" $ do
+            let exprs = [Assign "x" (BoolLit True), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
+            result <- runInterpreter Map.empty exprs
+            result `shouldBe` Right (IntVal 0, Map.fromList [("x", BoolVal True), ("y", IntVal 42)])
 
-      it "should consider an empty string falsy" $ do
-          let exprs = [Assign "x" (StrLit ""), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
-          result <- runInterpreter Map.empty exprs
-          result `shouldBe` Right (IntVal 0, Map.fromList [("x", StrVal ""), ("y", IntVal 0)])
-      
-      it "should consider a non-empty string truthy" $ do
-          let exprs = [Assign "x" (StrLit "hello"), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
-          result <- runInterpreter Map.empty exprs
-          result `shouldBe` Right (IntVal 0, Map.fromList [("x", StrVal "hello"), ("y", IntVal 42)])
-      
-      it "should evaluate if statement with false condition" $ do
-          let exprs = [Assign "x" (IntLit 0), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
-          result <- runInterpreter Map.empty exprs
-          result `shouldBe` Right (IntVal 0, Map.fromList [("x", IntVal 0), ("y", IntVal 0)])
+        it "should consider an empty string falsy" $ do
+            let exprs = [Assign "x" (StrLit ""), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
+            result <- runInterpreter Map.empty exprs
+            result `shouldBe` Right (IntVal 0, Map.fromList [("x", StrVal ""), ("y", IntVal 0)])
+        
+        it "should consider a non-empty string truthy" $ do
+            let exprs = [Assign "x" (StrLit "hello"), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
+            result <- runInterpreter Map.empty exprs
+            result `shouldBe` Right (IntVal 0, Map.fromList [("x", StrVal "hello"), ("y", IntVal 42)])
+        
+        it "should evaluate if statement with false condition" $ do
+            let exprs = [Assign "x" (IntLit 0), If (Var "x") [Assign "y" (IntLit 42)] [Assign "y" (IntLit 0)]]
+            result <- runInterpreter Map.empty exprs
+            result `shouldBe` Right (IntVal 0, Map.fromList [("x", IntVal 0), ("y", IntVal 0)])
+
+        it "should evaluate if statement with multiple statements" $ do 
+            let exprs = [Assign "x" (IntLit 1), If (Var "x") [Assign "y" (IntLit 42), Assign "z" (IntLit 43)] [Assign "y" (IntLit 0), Assign "z" (IntLit 0)]]
+            result <- runInterpreter Map.empty exprs
+            result `shouldBe` Right (IntVal 0, Map.fromList [("x", IntVal 1), ("y", IntVal 42), ("z", IntVal 43)])
+
+    describe "Value to string" $ do
+        it "should correctly format an integer" $ do
+            let exprs = IntVal 42
+            let result = valueToString exprs
+
+            result `shouldBe` "42"
+
+        it "should correctly format a string" $ do
+            let exprs = StrVal "hello"
+            let result = valueToString exprs
+
+            result `shouldBe` "\"hello\""
+
+        it "should correctly format boolean true" $ do
+            let exprs = BoolVal True
+            let result = valueToString exprs
+
+            result `shouldBe` "true"
+        
+        it "should correctly format boolean false" $ do
+            let exprs = BoolVal False
+            let result = valueToString exprs
+
+            result `shouldBe` "false"
+        
+        it "should correctly format a list" $ do
+            let exprs = ListVal [IntVal 1, IntVal 2, IntVal 3]
+            let result = valueToString exprs
+
+            result `shouldBe` "[1, 2, 3]"
+        
+        it "should correctly format a list with strings" $ do
+            let exprs = ListVal [StrVal "hello", StrVal "world"]
+            let result = valueToString exprs
+
+            result `shouldBe` "[\"hello\", \"world\"]"
+        
+        it "should correctly format a list with mixed types" $ do
+            let exprs = ListVal [IntVal 1, StrVal "hello", BoolVal True]
+            let result = valueToString exprs
+
+            result `shouldBe` "[1, \"hello\", true]"
+        
+        it "should be able to show a function" $ do
+            let exprs = FuncVal ["x", "y"] [Add (Var "x") (Var "y")] Map.empty
+            let result = valueToString exprs
+
+            result `shouldBe` "<function>"
+
+        it "should be able to show null" $ do
+            let exprs = NullVal
+            let result = valueToString exprs
+
+            result `shouldBe` "null"
 
     describe "Print statement" $ do
       it "should evaluate print statement" $ do
---
+++ test/ParserSpec.hs
@@ -0,0 +1,125 @@
+module ParserSpec (spec) where
+
+import Test.Hspec
+import SimpleParser
+import Data.Either (isLeft)
+
+spec :: Spec
+spec = context "SimpleParser" $ do
+    describe "Basic operations" $ do
+      it "should parse variable assigment" $ do
+        parseProgram "x = 42" `shouldBe` Right [Assign "x" (IntLit 42)]
+      it "should parse addition" $ do
+        parseProgram "x = 42 + 10" `shouldBe` Right [Assign "x" (Add (IntLit 42) (IntLit 10))]
+      it "should parse subtraction" $ do
+        parseProgram "x = 42 - 10" `shouldBe` Right [Assign "x" (Sub (IntLit 42) (IntLit 10))]
+      it "should parse multiplication" $ do
+        parseProgram "x = 42 * 10" `shouldBe` Right [Assign "x" (Mul (IntLit 42) (IntLit 10))]
+      it "should parse division" $ do
+        parseProgram "x = 42 / 10" `shouldBe` Right [Assign "x" (Div (IntLit 42) (IntLit 10))]
+      it "should parse string concatenation" $ do
+        parseProgram "x = \"hello\" + \"world\"" `shouldBe` Right [Assign "x" (Add (StrLit "hello") (StrLit "world"))]
+      it "should parse expression with parentheses" $ do
+        parseProgram "x = (42 + 10) * 2" `shouldBe` Right [Assign "x" (Mul (Add (IntLit 42) (IntLit 10)) (IntLit 2))]
+      it "should parse the true boolean" $ do
+        parseProgram "x = true" `shouldBe` Right [Assign "x" (BoolLit True)]
+      it "should parse the false boolean" $ do
+        parseProgram "x = false" `shouldBe` Right [Assign "x" (BoolLit False)]
+
+    describe "If statement" $ do
+      it "should parse if statement" $ do
+        parseProgram "if x { print x } else { print \"no x\" }" `shouldBe` Right [If (Var "x") [Print (Var "x")] [Print (StrLit "no x")]]
+      it "should parse if statement with multiple statements" $ do
+        parseProgram "if x { print x print x } else { print \"no x\" print \"no x\" }" `shouldBe` Right [If (Var "x") [Print (Var "x"), Print (Var "x")] [Print (StrLit "no x"), Print (StrLit "no x")]]
+      it "should parse if statement with nested if" $ do
+        parseProgram "if x { if y { print x } else { print y } } else { print \"no x\" }" `shouldBe` Right [If (Var "x") [If (Var "y") [Print (Var "x")] [Print (Var "y")]] [Print (StrLit "no x")]]
+      it "should parse if statement with no else" $ do
+        parseProgram "if x { print x }" `shouldBe` Right [If (Var "x") [Print (Var "x")] []]
+    
+    describe "Print statement" $ do
+      it "should parse print statement" $ do
+        parseProgram "print x" `shouldBe` Right [Print (Var "x")]
+      it "should give error when print statement used without parameter" $ do
+        parseProgram "print" `shouldSatisfy` isLeft 
+
+    describe "Comparison operators" $ do
+      it "should parse equality operator (==) for integers" $ do
+        parseProgram "x = 42 == 42" `shouldBe` Right [Assign "x" (Eq (IntLit 42) (IntLit 42))]
+      it "should parse equality operator (==) for strings" $ do
+        parseProgram "x = \"hello\" == \"hello\"" `shouldBe` Right [Assign "x" (Eq (StrLit "hello") (StrLit "hello"))]
+      it "should parse inequality operator (!=) for integers" $ do
+        parseProgram "x = 42 != 43" `shouldBe` Right [Assign "x" (Neq (IntLit 42) (IntLit 43))]
+      it "should parse inequality operator (!=) for strings" $ do
+        parseProgram "x = \"hello\" != \"world\"" `shouldBe` Right [Assign "x" (Neq (StrLit "hello") (StrLit "world"))]
+      it "should parse less than operator (<) for integers" $ do
+        parseProgram "x = 42 < 43" `shouldBe` Right [Assign "x" (Lt (IntLit 42) (IntLit 43))]
+      it "should parse greater than operator (>) for integers" $ do
+        parseProgram "x = 43 > 42" `shouldBe` Right [Assign "x" (Gt (IntLit 43) (IntLit 42))]
+      it "should parse less than or equal operator (<=) for integers" $ do
+        parseProgram "x = 42 <= 43" `shouldBe` Right [Assign "x" (Le (IntLit 42) (IntLit 43))]
+      it "should parse greater than or equal operator (>=) for integers" $ do
+        parseProgram "x = 43 >= 42" `shouldBe` Right [Assign "x" (Ge (IntLit 43) (IntLit 42))]
+
+    describe "List operations" $ do
+      it "should parse list literal" $ do
+        parseProgram "x = [1, 2, 3]" `shouldBe` Right [Assign "x" (ListLit [IntLit 1, IntLit 2, IntLit 3])]
+      it "should parse list access" $ do
+        parseProgram "x = [1, 2, 3][0]" `shouldBe` Right [Assign "x" (ListAccess (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 0))]
+      it "should parse list append" $ do
+        parseProgram "x = [1, 2, 3] << 4" `shouldBe` Right [Assign "x" (ListAppend (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 4))]
+      it "should parse list remove" $ do
+        parseProgram "x = [1, 2, 3] >> 2" `shouldBe` Right [Assign "x" (ListRemove (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 2))]
+      it "should parse list pop" $ do
+        parseProgram "x = [1, 2, 3] =>> 0" `shouldBe` Right [Assign "x" (ListPop (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 0))]
+      it "should parse list add" $ do
+        parseProgram "x = [1, 2, 3] << 0 <<= 4" `shouldBe` Right [Assign "x" (ListAdd (ListLit [IntLit 1, IntLit 2, IntLit 3]) (IntLit 0) (IntLit 4))]
+
+    describe "Comments" $ do
+      it "should parse single line comment" $ do
+        parseProgram "# This is a comment" `shouldBe` Right [Comment "This is a comment"]
+      it "should parse multi line comment" $ do
+        parseProgram "/* This is a comment */" `shouldBe` Right [MultiLineComment "This is a comment"]
+      it "should parse multi line comment with new lines" $ do
+        parseProgram "/* This is a \n comment */" `shouldBe` Right [MultiLineComment "This is a \n comment"]
+      it "should parse a comment with a # in it" $ do
+        parseProgram "# This is a comment with a # in it" `shouldBe` Right [Comment "This is a comment with a # in it"]
+      it "should parse a comment with a /* in it" $ do
+        parseProgram "# This is a comment with a /* in it" `shouldBe` Right [Comment "This is a comment with a /* in it"]
+      it "should parse a comment with a */ in it" $ do
+        parseProgram "# This is a comment with a */ in it" `shouldBe` Right [Comment "This is a comment with a */ in it"]        
+      it "should correctly parse inline comments on the side of a statement" $ do
+        parseProgram "x = 42 # This is a comment" `shouldBe` Right [Assign "x" (IntLit 42), Comment "This is a comment"]
+
+    describe "Function definition" $ do
+      it "should parse a function definition" $ do
+        parseProgram "def add(x, y) { return x + y }" `shouldBe` Right [FuncDef "add" ["x", "y"] [Return (Add (Var "x") (Var "y"))]]
+
+      it "should fail if the function definition is missing the return keyword" $ do
+        parseProgram "def add(x, y) { x + y }" `shouldSatisfy` isLeft
+      
+      it "should fail if the return keyword is used outside a function" $ do
+        parseProgram "return 42" `shouldSatisfy` isLeft
+
+    describe "Function call" $ do
+      it "should parse a function call as a statement" $ do
+        parseProgram "add(1, 2)" `shouldBe` Right [FuncCall "add" [IntLit 1, IntLit 2]]
+      it "should parse a function call as an expression" $ do
+        parseProgram "x = add(1, 2)" `shouldBe` Right [Assign "x" (FuncCall "add" [IntLit 1, IntLit 2])]
+
+    describe "Logical operations" $ do
+      it "should be able to parse the logical and operator (and)" $ do
+        parseProgram "x = true and false" `shouldBe` Right [Assign "x" (LogicAnd (BoolLit True) (BoolLit False))]
+      it "should be able to parse the logical or operator (or)" $ do
+        parseProgram "x = true or false" `shouldBe` Right [Assign "x" (LogicOr (BoolLit True) (BoolLit False))]
+      it "should be able to parse the logical not operator (not)" $ do
+        parseProgram "x = not true" `shouldBe` Right [Assign "x" (LogicNot (BoolLit True))]
+
+    describe "For loop" $ do
+      it "should parse a for loop" $ do
+        parseProgram "for (x = 0; x < 2; x = x + 1) {}" `shouldBe` Right [ForLoop (Assign "x" (IntLit 0)) (Lt (Var "x") (IntLit 2)) (Assign "x" (Add (Var "x") (IntLit 1))) []]
+
+    describe "Import module" $ do
+      it "should parse an import module statement" $ do
+        parseProgram "import module" `shouldBe` Right [ImportModule "module"]
+
+ 
